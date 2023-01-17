4a56222ae8efe8c8bbbbebb7a753fb3af04fe251
Sl4vP0weR <42337892+Sl4vP0weR@users.noreply.github.com>
2023-01-18 01:43:15 +0800

    Fix ambiguous reference.

--- RicochetSoundDefinitions.cs
+++ RicochetSoundDefinitions.cs
@@ -6,7 +6,7 @@ public static class RicochetSoundDefinitions
     public static SoundDef GetRicochetSound(int id) => DefDatabase<SoundDef>.GetNamed(RicochetSoundPrefix + id);
     static int? cachedCount;
     public static int RicochetSoundsCount => cachedCount ??= DefDatabase<SoundDef>.AllDefs.Count(x => x.defName.StartsWith(RicochetSoundPrefix));
-    public static SoundDef GetRandomRicochetSound() => GetRicochetSound(Random.Range(1, RicochetSoundsCount));
+    public static SoundDef GetRandomRicochetSound() => GetRicochetSound(UnityEngine.Random.Range(1, RicochetSoundsCount));
     public static void PlayRandomFor(Bullet bullet)
     {
         try
